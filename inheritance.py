c8483766571d7a755ca06a8172d8ceeb87e4fa29
Samp1997 <130931137+Samp1997@users.noreply.github.com>
2023-07-18 19:32:40 -0700

    Add files via upload

---
+++ inheritance.py
@@ -0,0 +1,11 @@
+#name of the class
+class People:
+    name = 'sam'
+    Age = 26
+    height = '6ft 5in'
+#The info class inherits from the People class
+class info(People):
+    #Attributes
+    hometown = 'Las Vegas'
+    highschool = 'Green Valley high school'
+
