c450cc4b7d521fce16aec6fb9a253f2be2eaa013
Samp1997 <130931137+Samp1997@users.noreply.github.com>
2023-07-19 19:52:48 -0700

    Add files via upload

--- inheritance.py
+++ inheritance.py
@@ -1,18 +1,36 @@
-#name of the class
-class People:
+#name of the parent class
+class man:
     name = 'sam'
     Age = 26
     height = '6ft 5in'
-#The info class inherits from the People class
-class info(People):
+
+    def information(self):
+        msg = "Name: {}\n Age: {} \nHeight: 6ft 6in"
+        return msg
+
+#child class
+class info(man):
     #Attributes
     hometown = 'Las Vegas'
     highschool = 'Green Valley high school'
 
-# child class that inherits the parent class
-class Man(People):
-    name = 'sam'
-    Age = 26
-    height = '6ft 5in'
+    def information(self):
+        msg = "\nI grew up in Las Vegas, and went to Greenvalley high school"
+        return msg
 
-print(Man)
+    
+
+# child class 
+class People(info):
+    M = 'Man'
+    W = 'Women'
+
+    def human(self):
+            msg = "I am a man and not a women"
+            return msg
+    
+
+if __name__ == "__main__":
+    man = man()
+    print(man.information())
+    print(info.information())
