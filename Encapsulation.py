6101ef0b6edd5fb5c864616466ef18bb7627f7c0
Samp1997 <130931137+Samp1997@users.noreply.github.com>
2023-07-27 19:46:28 -0700

    Add files via upload

--- Encapsulation.py
+++ Encapsulation.py
@@ -3,13 +3,17 @@ class Protected:
     def __init__(self):
         self.__privateVar = 50
 # child class
-    def getPrivate(self):
+    def getPrivate(self, name, number):
+        self.__name = name
+        self.__number = number
         print(self.__privateVar)
-
+         
     def setPrivate(self, private):
         self.__privateVar = private
+        print(private.self)
 # the private variable that is set to be retreved from the classes above
 obj = Protected()
 obj.getPrivate()
 obj.setPrivate(25)
 obj.getPrivate()
+obk.getPrivate(name,number)
